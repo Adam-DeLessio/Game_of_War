dc0583ef0970209ed59d2ae1ee7c7475c8dcfba0
Adam DeLessio <adamdelessio@gmail.com>
2019-11-23 14:17:05 -0500

    Basic comparisons working and displaying

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 
-function Card(rank, suit, value) {
-	this.rank = rank
-	this.suit = suit
-	this.value = value
-}
-
 function Deck() {
 	this.cards = []
 
@@ -21,6 +15,12 @@ function Deck() {
 		}
 	}
 	Shuffle(this.cards)
+}
+
+function Card(rank, suit, value) {
+	this.rank = rank
+	this.suit = suit
+	this.value = value
 }
 
 function Shuffle(cards) {
@@ -41,9 +41,38 @@ function Deal(cards) {
 	let player1 = cards.splice(0, 26)
 	let player2 = cards.splice(0, 26)
 
-	console.log(player1)
-	console.log(player2)
+	Play(player1, player2)
 }
+
+
+
+
+function Play(player1, player2) {
+	this.player1deck = player1
+	this.player2deck = player2
+
+	let player1card = player1deck.shift()
+	let player2card = player2deck.shift()
+
+	if (player1card.value > player2card.value) {
+		player1deck.push(player1card, player2card)
+		console.log("Player 1 played " + player1card.rank + " of " + player1card.suit + ", and now has " + player1deck.length + " cards")
+		console.log("Player 2 played " + player2card.rank + " of " + player2card.suit + ", and now has " + player2deck.length + " cards")
+		console.log("Player 1 wins round")
+	} else if (player1card.value < player2card.value) {
+		player2deck.push(player1card, player2card)
+		console.log("Player 1 played " + player1card.rank + " of " + player1card.suit + ", and now has " + player1deck.length + " cards")
+		console.log("Player 2 played " + player2card.rank + " of " + player2card.suit + ", and now has " + player2deck.length + " cards")
+		console.log("Player 2 wins round")
+	}
+}
+
+
+
+
+
+
+
 
 
 let deck1 = new Deck
@@ -65,79 +94,22 @@ let deck1 = new Deck
 
 
 
-// class Round {
-// 	constructor(player1card, player2card) {
-// 		this.player1card = player1card
-// 		this.player2card = player2card
 
-// 		if (player1card > player2card) {
-// 			player1empty.unshift(player1card, player2card)
-// 			console.log("Player 1 wins round")
-// 		} else if (player1card < player2card) {
-// 			player2empty.unshift(player1card, player2card)
-// 			console.log("Player 2 wins round")
-// 		} else if (player1card === player2card) {
-// 			let player1down = player1deck.splice(0, 2)
-// 			let player2down = player2deck.splice(0, 2)
+		// else if (player1card === player2card) {
+		// 	let player1down = player1deck.splice(0, 2)
+		// 	let player2down = player2deck.splice(0, 2)
 
-// 			let player1war = player1deck.shift()
-// 			let player2war = player2deck.shift()
+		// 	let player1war = player1deck.shift()
+		// 	let player2war = player2deck.shift()
 
-// 			if (player1war > player2war) {
-// 				player1empty.unshift(player1down, player2down, player1card, player2card)
-// 				console.log("Player 1 wins round")
-// 			} else if (player1card < player2card) {
-// 				player2empty.unshift(player1down, player2down, player1card, player2card)
-// 				console.log("Player 2 wins round")
-// 			}
-// 		}
-// 	}
-// }
-
-
-
-
-// while (player1deck.length < 26) {
-// 	let random = Math.floor(Math.random() * 52) + 1;
-// 	if (player1deck.indexOf(random) === -1) {
-// 		player1deck.push(deck.cards[random]);
-// 	}
-// }
-
-
-
-
-// class Card {
-// 	constructor(rank, suit, value) {
-// 		this.rank = rank
-// 		this.suit = suit
-// 		this.value = value
-// 	}
-// }
-
-
-// class Deck {
-// 	constructor() {
-// 		this.cards = []
-
-// 		const ranks = ["2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"]
-// 		const suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
-
-// 		for (let i = 0; i < ranks.length; i++) {
-// 			for (let j = 0; j < suits.length; j++) {
-// 				let rank = ranks[i]
-// 				let suit = suits[j]
-// 				let value = i;
-
-// 				this.cards.push(new Card(rank, suit, value))
-// 			}
-// 		}
-// 	}
-// }
-
-
-
-
+		// 	if (player1war > player2war) {
+		// 		player1empty.unshift(player1down, player2down, player1card, player2card)
+		// 		console.log("Player 1 wins round")
+		// 	} else if (player1card < player2card) {
+		// 		player2empty.unshift(player1down, player2down, player1card, player2card)
+		// 		console.log("Player 2 wins round")
+		// 	}
+		// }
 
 
 
