7d50ac0874f480e579335805f4910dd2d7935429
Adam DeLessio <adamdelessio@gmail.com>
2019-11-23 16:32:59 -0500

    Progress on three card war

--- script.js
+++ script.js
@@ -38,36 +38,89 @@ function Shuffle(cards) {
 }
 
 function Deal(cards) {
-	let player1 = cards.splice(0, 26)
-	let player2 = cards.splice(0, 26)
+	let player1deck = cards.splice(0, 26)
+	let player2deck = cards.splice(0, 26)
 
-	Play(player1, player2)
+	Play(player1deck, player2deck)
 }
 
 
+function Play(player1deck, player2deck) {
+	this.player1deck = player1deck
+	this.player2deck = player2deck
 
-
-function Play(player1, player2) {
-	this.player1deck = player1
-	this.player2deck = player2
 
 	let player1card = player1deck.shift()
 	let player2card = player2deck.shift()
 
+	let player1warStack = []
+	let player2warStack = []
+
+	console.log("Player 1 played " + player1card.rank + " of " + player1card.suit)
+	console.log("Player 2 played " + player2card.rank + " of " + player2card.suit)
 	if (player1card.value > player2card.value) {
 		player1deck.push(player1card, player2card)
-		console.log("Player 1 played " + player1card.rank + " of " + player1card.suit + ", and now has " + player1deck.length + " cards")
-		console.log("Player 2 played " + player2card.rank + " of " + player2card.suit + ", and now has " + player2deck.length + " cards")
 		console.log("Player 1 wins round")
+		console.log("Player 1 now has " + player1deck.length + " cards")
+		console.log("Player 2 now has " + player2deck.length + " cards")
+		Play(player1deck, player2deck)
 	} else if (player1card.value < player2card.value) {
 		player2deck.push(player1card, player2card)
-		console.log("Player 1 played " + player1card.rank + " of " + player1card.suit + ", and now has " + player1deck.length + " cards")
-		console.log("Player 2 played " + player2card.rank + " of " + player2card.suit + ", and now has " + player2deck.length + " cards")
 		console.log("Player 2 wins round")
+		console.log("Player 1 now has " + player1deck.length + " cards")
+		console.log("Player 2 now has " + player2deck.length + " cards")
+		Play(player1deck, player2deck)
+	} else if (player1card.value === player2card.value) {
+		console.log("War!")
+		war(player1card, player2card, player1deck, player2deck, player1warStack, player2warStack)
 	}
 }
 
 
+
+
+
+function war(player1card, player2card, player1deck, player2deck, player1warStack, player2warStack) {
+	this.player1card = player1card
+	this.player2card = player2card
+	this.player1deck = player1deck
+	this.player2deck = player2deck
+	this.player1warStack = player1warStack
+	this.player2warStack = player2warStack
+
+
+		player1warStack.push(player1card)
+		player2warStack.push(player2card)
+
+		player1warStack.push(player1deck.splice(0, 3))
+		player2warStack.push(player2deck.splice(0, 3))
+
+		player1card = player1deck.shift()
+		player2card = player2deck.shift()
+
+		console.log("Player 1 played " + player1card.rank + " of " + player1card.suit)
+		console.log("Player 2 played " + player2card.rank + " of " + player2card.suit)
+		if (player1card.value > player2card.value) {
+			player1deck.push(player1card, player2card)
+			player1deck = player1deck.concat(player1warStack, player2warStack)
+			console.log("Player 1 wins round")
+			console.log("Player 1 now has " + player1deck.length + " cards")
+			console.log("Player 2 now has " + player2deck.length + " cards")
+			Play(player1deck, player2deck)
+		} else if (player1card.value < player2card.value) {
+			player2deck.push(player1card, player2card)
+			player2deck = player2deck.concat(player1warStack, player2warStack)
+			console.log("Player 2 wins round")
+			console.log("Player 1 now has " + player1deck.length + " cards")
+			console.log("Player 2 now has " + player2deck.length + " cards")
+			Play(player1deck, player2deck)
+		} else if (player1card.value === player2card.value) {
+			console.log("Player 1 played " + player1war.rank + " of " + player1war.suit)
+			console.log("Player 2 played " + player2war.rank + " of " + player2war.suit)
+			console.log("War Again!")
+			war(player1card, player2card, player1deck, player2deck, player1warStack, player2warStack)
+		}
+	}
 
 
 
@@ -81,39 +134,15 @@ let deck1 = new Deck
 
 
 
+/// Code for tie breaker without WAR
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-		// else if (player1card === player2card) {
-		// 	let player1down = player1deck.splice(0, 2)
-		// 	let player2down = player2deck.splice(0, 2)
-
-		// 	let player1war = player1deck.shift()
-		// 	let player2war = player2deck.shift()
-
-		// 	if (player1war > player2war) {
-		// 		player1empty.unshift(player1down, player2down, player1card, player2card)
-		// 		console.log("Player 1 wins round")
-		// 	} else if (player1card < player2card) {
-		// 		player2empty.unshift(player1down, player2down, player1card, player2card)
-		// 		console.log("Player 2 wins round")
-		// 	}
-		// }
-
-
-
-
+	// } else if (player1card.value === player2card.value) {
+	// 	player1deck.push(player1card)
+	// 	player2deck.push(player2card)
+	// 	console.log("Draw")
+	// 	console.log("Player 1 played " + player1card.rank + " of " + player1card.suit + ", and now has " + player1deck.length + " cards")
+	// 	console.log("Player 2 played " + player2card.rank + " of " + player2card.suit + ", and now has " + player2deck.length + " cards")
+	// }
 
 
 
